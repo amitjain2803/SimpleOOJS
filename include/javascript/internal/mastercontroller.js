680c7f81b5ebb9618ec1b97069a828ba93762902
Amit Jain <amitjain2803@icloud.com>
2017-10-03 10:51:41 +0530

    First Commit

---
+++ include/javascript/internal/mastercontroller.js
@@ -0,0 +1,14 @@
+(function(app) {
+
+    console.log($('#myname'))
+
+    $(document).ready(function() {
+
+        console.log('My App has started')
+            //$('#myname').html('This is my name');
+        console.log($('#myname'))
+    })
+
+})(myApp = myApp || {});
+
+var myApp
